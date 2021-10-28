531b85aaabcde395168322298b0df37e0eff1513
AymericPost <aymeric.post@gmail.com>
2021-10-28 14:33:12 +0200

    Added step counter label

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const levels = new Campaign("./levels");
 const soko = new Sokoban(levels.next());
 soko.render();
 
+const stepLabel = new Gtk.Label();
+stepLabel.label = "Steps: " + soko.steps;
+
 const upButton = new Gtk.Button({ label: "↑" });
 const leftButton = new Gtk.Button({ label: "←" });
 const rightButton = new Gtk.Button({ label: "→" });
@@ -40,6 +43,7 @@ controlsGrid.attach(restartButton, 0, 5, 3, 1);
 controlsBox.packStart(controlsGrid, true, false, 2);
 
 mainBox.packStart(gameLabel, true, false, 0);
+mainBox.packStart(stepLabel, true, false, 0);
 mainBox.packStart(soko.gridBox, true, false, 0);
 mainBox.packStart(controlsBox, true, false, 0);
 
@@ -50,26 +54,32 @@ Gtk.main();
 
 function onUp() {
     soko.moveUp() && nextLevel();
+    stepLabel.label = "Steps: " + soko.steps;
 
 }
 
 function onDown() {
     soko.moveDown() && nextLevel();
+    stepLabel.label = "Steps: " + soko.steps;
 }
 
 function onLeft() {
-    soko.moveLeft() && nextLevel();;
+    soko.moveLeft() && nextLevel();
+    stepLabel.label = "Steps: " + soko.steps;
 }
 
 function onRight() {
     soko.moveRight() && nextLevel();
+    stepLabel.label = "Steps: " + soko.steps;
 }
 
 function onRestart() {
     soko.clear();
+    soko.steps -= soko.currentSteps;
 
     soko.parseLayout(soko.rawLayout);
     soko.render();
+    stepLabel.label = "Steps: " + soko.steps;
 }
 
 function nextLevel() {
@@ -85,9 +95,13 @@ function nextLevel() {
         leftButton.on("clicked", () => {Gtk.mainQuit()});
         rightButton.on("clicked", () => {Gtk.mainQuit()});
         downButton.on("clicked", () => {Gtk.mainQuit()});
-        gameLabel.label = "Congratulations!";
-        restartButton.label = "Close";
         restartButton.on("clicked", () => {Gtk.mainQuit()});
+        restartButton.label = "Close";
+
+        gameLabel.label = "Congratulations!";
+        stepLabel.label = "Game completed in " + soko.steps + " steps.";
+        
+        
         win.showAll();
     }
 }
